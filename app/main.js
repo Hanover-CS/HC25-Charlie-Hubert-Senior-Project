fe69cdb960b37f61b4ab24a2c69c8ad409b232a0
Charlie Hubert <112531710+CharlieHubert@users.noreply.github.com>
2024-09-12 16:42:06 +0000

    Added open Layer and have the map set to center on Hanover college

---
+++ app/main.js
@@ -0,0 +1,18 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj.js';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: fromLonLat([-85.462007, 38.716167]),
+    zoom: 16.3,
+  })
+});

136d78c553f38346021ba2da8ff05dec70a61a78
Charlie Hubert <112531710+CharlieHubert@users.noreply.github.com>
2024-12-09 03:58:45 +0000

    implemented more features and a hard coded route box

--- main.js
+++ main.js
@@ -15,7 +15,11 @@ import stadium from "./assets/images/stadium.png";
 import soccerField from "./assets/images/soccerField.png";
 import tennis from "./assets/images/tennis.png";
 import baseball from "./assets/images/baseball.png";
+import softball from "./assets/images/softball.png";
 import phidelt from "./assets/images/phidelt.png"
+import lambda from "./assets/images/lambda.png"
+import sigma from "./assets/images/sigma.png";
+import fiji from "./assets/images/fiji.png";
 import dining from "./assets/images/dining.png"
 import residence from "./assets/images/residencehall.png"
 import library from "./assets/images/library.png"
@@ -23,6 +27,8 @@ import stadiumSidebar from "./assets/images/stadiumSidebar.png";
 import soccerSidebar from "./assets/images/soccerSidebar.png";
 import tennisSidebar from "./assets/images/tennisSidebar.png";
 import phiDeltaThetaSidebar from "./assets/images/phideltSidebar.jpg";
+import sigmachiSidebar from "./assets/images/sigmachiSidebar.jpg";
+import fijiSidebar from "./assets/images/fijiSidebar.jpg";
 import shoeboxSidebar from "./assets/images/shoeboxSidebar.jpg";
 import greenwoodSidebar from "./assets/images/greenwoodSidebar.jpg";
 import dugganLibrarySidebar from "./assets/images/librarySidebar.jpg";
@@ -30,7 +36,11 @@ import coulterSidebar from "./assets/images/coulterSidebar.jpg";
 import blytheSidebar from "./assets/images/blytheSidebar.jpg";
 import donnerSidebar from "./assets/images/donnerSidebar.jpg";
 import croweSidebar from "./assets/images/croweSidebar.jpg";
+import wileySidebar from "./assets/images/wileySidebar.jpg";
+import ideSidebar from "./assets/images/ideSidebar.jpg";
+import kpSidebar from "./assets/images/kpSidebar.jpg";
 import baseballSidebar from "./assets/images/baseballSidebar.jpg";
+import softballSidebar from "./assets/images/softballSidebar.jpg";
 import { soccerSchedule, footballSchedule, tennisSchedule, displayGames } from './schedule.js';
 import { createFeature, setFeatureStyle } from './features';
 import {coords} from './coords';
@@ -40,14 +50,17 @@ const stadiumFeature = createFeature("Alumni Stadium", "Athletics", coords.stadi
 const soccerFeature = createFeature("Soccer Fields", "Athletics", coords.soccer, 'https://athletics.hanover.edu/sports/mens-soccer/schedule/2024', soccerSidebar, "The men's and women's soccer programs use two natural turf fields, including a practice and a game field, and also host occasional matches on Alumni Stadium's artificial surface. <br><br> The game field also features the Hagenah Press Box, sheltered benches for each team, a scoreboard at each end of the field, and seating down the far side.");
 const tennisFeature = createFeature("Zeddies Tennis Center", "Athletics", coords.tennis, 'https://athletics.hanover.edu/sports/mens-tennis/schedule/2023-24', tennisSidebar, "Completed in 2012, the Zeddies Tennis Center, made possible by a gift from Michael ’77 and Judy Zeddies, features a total of eight courts.<br><br>In addition to the eight courts, Zeddies Tennis Center also consists of an adjacent lighted pavilion and storage space. <br><br> Part of the Panther Athletic Complex, Zeddies Tennis Center sits directly behind Alumni Stadium and is adjacent to grass practice fields that are available for the College’s outdoor teams and intramural sports.");
 const baseballFeature = createFeature("K.T. Young Ballpark", "Athletics", coords.baseball, 'https://athletics.hanover.edu/sports/baseball/schedule/2024', baseballSidebar,"Scenic K.T. Young Ballpark, home to the Panthers since 1972, is one of the nicest in the Midwest. The field saw recent improvements in 2014 when professional style dugouts and bullpens were added along with a press box, a new scoreboard, and two 70-foot batting cages down the left field line. It features a crushed red brick and natural turf infield and an outfield fence lined with more than 100 trees. The field also has a sprinkler system throughout the field.<br><br>The park is named after Kenneth T. Young '65 and in memory of Thomas D. Young '39 for their participation in and support of athletic programs at Hanover College.<br><br>The field is named Dick Naylor Field in honor of former head baseball coach Dick Naylor for his outstanding service to Hanover College and his accomplishments as head baseball coach for 36 years.<br><br>The Gaunt Grandstands and Press Box are named in honor of Jim '67 and Kate Gaunt for their special interest in the students of Hanover College and for their generous financial support to the athletic facilities project of the Live Our Loyalty Campaign. " )
-//const softballFeature = 
+const softballFeature = createFeature("Kops-Bedel Stadium", "Athletics", coords.softball, 'https://athletics.hanover.edu/sports/softball', softballSidebar, "Hanover's softball team plays on one of the Midwest's finest fields. Kops-Bedel Stadium features a crushed red-brick infield and an agrilime warning track in the outfield along with professional style dugouts, bullpens for each side, batting cages, and a press box.<br><br>The stadium is named in honor of Elaine E. Kops-Bedel '74 and Eric J. Bedel in recognition of her special interest in the women students of Hanover College, her leadership in securing financial support for athletic facilities, and their generous contributions to the Live Our Loyalty Campaign.<br><br>The field is named Brouillette Field for Donald G. & Marilyn Brouillette by their daughter, JoAnn Brouillette '83 in honor of their inspiration and impact on family, community, Hanover College, and women's sports.<br><br>The Rector-Gleason Grandstands and Press Box are named in honor of Greg '72 and Kristine Rector Gleason '73 in recognition of their support of the Live Our Loyalty Campaign of Hanover College.");
 //const basketballFeature = 
 
 // dining features
 const shoeboxFeature = createFeature("The Shoebox", "Dining", coords.shoebox, 'https://hanoverdining.catertrax.com/shopportal.asp?pageref=WeeklyMenu&intOrderID=&intCustomerID=', shoeboxSidebar, "The Shoebox is a place where you can kick back and relax with friends while enjoying various activities such as- singing karaoke, listening to DJs and bands or watching sports and your favorite TV shows. <br><br>The menu includes items such as: wraps, burgers, wings/tenders, quesadillas, salads, shakes and more! We also offer beer and wine to those who are 21 years of age and over.<br><br>ACADEMIC YEAR HOURS <br>Monday: Closed<br><br>Tuesday - Sunday: 5pm — 10pm<br><br>Meal Exchange Hours: Tuesday - Sunday: 5pm — 10pm<br>(All Plans limited to 1 swipe per Retail Meal Period)");
 
 // greek features
-const phiDeltaThetaFeature = createFeature("Phi Delta Theta", "Greek", coords.PhiDeltaTheta, 'https://hanover.phideltatheta.org/', phiDeltaThetaSidebar, "Phi Delta Theta is a prominent fraternity at Hanover College, focused on leadership and academic success. The fraternity provides a supportive community for its members.");
+const phiDeltaThetaFeature = createFeature("Phi Delta Theta", "Greek", coords.PhiDeltaTheta, 'https://hanover.phideltatheta.org/', phiDeltaThetaSidebar, "Phi Delta Theta (ΦΔΘ) is a fraternity founded in 1848 with a commitment to the principles of friendship, sound learning, and moral rectitude. The fraternity focuses on fostering personal growth, leadership, academic achievement, and brotherhood among its members. Phi Delta Theta aims to develop men of strong character who contribute positively to their communities and uphold high standards of integrity. The fraternity is known for its dedication to creating lasting relationships and providing a supportive environment for members' success both during college and beyond.<br><br>Phi Delta Theta is a prominent fraternity at Hanover College, focused on leadership and academic success. The fraternity provides a supportive community for its members.");
+const sigmaChiFeature = createFeature("Sigma Chi Fraternity", "Greek", coords.SigmaChi, '', sigmachiSidebar,"Sigma Chi Alpha is a local fraternity that focuses on building strong bonds of brotherhood, leadership, and personal development. It emphasizes academic achievement, community service, and fostering a supportive environment for its members. The fraternity strives to cultivate values of integrity, respect, and responsibility, while encouraging members to lead by example and contribute positively to society.")
+const lambdaChiAlphaFeature = createFeature("Lambda Chi Fraternity", "Greek", coords.Lambda, '', lambda, "Lambda Chi Alpha is a fraternity founded in 1909 that values friendship, leadership, scholarship, and service. It aims to create lifelong bonds of brotherhood and foster personal growth through community involvement and philanthropy. The fraternity emphasizes inclusivity, personal development, and the importance of making a positive impact on both campus and society.")
+const fijiFeature = createFeature("Phi Gamma Delta Fraternity", "Greek", coords.Fiji, '', fijiSidebar, "Fiji, or Phi Gamma Delta, is a fraternity founded in 1848 with a strong focus on friendship, knowledge, service, morality, and excellence. Its core values emphasize leadership, academic success, and the development of lifelong relationships among members. The fraternity encourages a commitment to community involvement, personal growth, and high ethical standards, aiming to create a positive impact both on campus and in the broader world.")
 
 // residence features
 const greenwoodSuitesFeature = createFeature("Greenwood Suites", "Residence Hall", coords.greenwood, '', greenwoodSidebar, "HOUSES: 72 STUDENTS <br><br>AVERAGE ROOM SIZE: 14’1″ X 12’4″ <br><br>Students living in the Greenwood Suites are more autonomous with only upper-class students living in the complex. The individual suites in Greenwood house students in a suite of three bedrooms. Two bedrooms share a bath, while the third has its own bathroom. Two lower-level suites have roll-in showers to make them handicap-accessible. Each suite has a small efficiency kitchen with a compact refrigerator and a sink. A range is not allowed; however, the residents may bring in a microwave. All suites are air-conditioned with full laundry facilities available onsite.");
@@ -55,14 +68,16 @@ const coulterFeature = createFeature("Coulter House", "Residence Hall", coords.c
 const croweFeature = createFeature("Crowe Hall", "Residence Hall", coords.crowe, '', croweSidebar, "HOUSES: 120 STUDENTS<br><br>AVERAGE ROOM SIZE: 13’5″ X 13’3″<br><br>Crowe Hall is one of the suite-style residence halls for first-year and upper-class residents, accommodating 120 students. There are common bathrooms for each wing, common lounges and laundry facilities." );
 const donnerFeature = createFeature("Donner Hall", "Residence Hall", coords.donner, '', donnerSidebar, "HOUSES: 120 FIRST-YEAR STUDENTS<br><br>AVERAGE ROOM SIZE: 13’1″ X 12′<br><br>First-year living featuring primarily double rooms. There are two common bathrooms per floor, one for each wing, common floor lounges and laundry facilities. This hall is co-ed by floor.<br>Donner Hall is undergoing a massive renovation in 2021 with new bathrooms, laundry room and yoga and circuit training rooms. It is also ADA-accessible via a new entrance." );
 const blytheFeature = createFeature("Blythe Hall", "Residence Hall", coords.blythe, '', blytheSidebar, "HOUSES: 40 FIRST-YEAR STUDENTS<br><br>AVERAGE ROOM SIZE: 13′ X 13′2″<br><br>Blythe features double rooms. Each floor has a common bathroom. The basement also houses a common lounge, kitchen and laundry facilities." );
-
-
+const wileyFeature = createFeature("Wiley Hall", "Residence Hall", coords.wiley, '', wileySidebar, "HOUSES: 90 STUDENTS<br><br>AVERAGE ROOM SIZE: 14’1″ X 12’4″<br><br>Wiley Hall is a traditional upper-class residence hall, accommodating 90 students in mostly single rooms. There are common bathrooms for each wing, common lounges and laundry facilities." );
+const ideFeature = createFeature("Ide Hall", "Residence Hall", coords.ide, '', ideSidebar, "HOUSES: 40 FIRST-YEAR STUDENTS<br><br>AVERAGE ROOM SIZE: 13’1″ X 13’3″<br><br>Ide Hall is a first-year residence hall with double rooms and is co-ed by floor. There are common bathrooms for each wing, common lounges and laundry facilities.<br><br>Ide just underwent a renovation in 2020 and is practically new from top to bottom, including air-conditioning in all rooms.");
+const kpFeature = createFeature("Katherine Parker Hall", "Residence Hall", coords.kp, '', kpSidebar, "HOUSES: 90 FIRST-YEAR STUDENTS<br><br>AVERAGE ROOM SIZE: 13’11” X 10’8″<br><br>Katharine Parker Hall is for first-year students, accommodating 90 students with men and women living on separate floors.<br><br>There are common bathrooms for each wing, a common lounge and laundry facilities.");
 // Academic features
 const dugganLibraryFeature = createFeature("Duggan Library", "Library", coords.library, 'https://www.hanover.edu/library', dugganLibrarySidebar, "Duggan Library is the main library at Hanover College, offering a wide selection of books, journals, and digital resources for students and faculty.<br><br>The first floor remains open until closing time. The second and third floors close 15 minutes prior to the listed closing time.<br><br>Monday - Thursday: 7:30 a.m. - 10:00 p.m.<br>Friday: 7:30 a.m. - 5:00 p.m.<br>Saturday: noon - 5:00 p.m.<br>Sunday: 1:00 p.m. - 10:00 p.m.");
 
 
-const featuresArray = [soccerFeature, stadiumFeature, tennisFeature, baseballFeature, phiDeltaThetaFeature, shoeboxFeature, greenwoodSuitesFeature, dugganLibraryFeature, coulterFeature,
-  blytheFeature, croweFeature, donnerFeature
+const featuresArray = [soccerFeature, stadiumFeature, tennisFeature, softballFeature, baseballFeature, phiDeltaThetaFeature, lambdaChiAlphaFeature,
+  sigmaChiFeature, fijiFeature, shoeboxFeature, greenwoodSuitesFeature, dugganLibraryFeature, coulterFeature,
+  blytheFeature, croweFeature, donnerFeature, wileyFeature, ideFeature, kpFeature
 ];
 // for feature of array feature name : feature
 
@@ -109,7 +124,11 @@ setFeatureStyle(stadiumFeature, stadium, map.getView().getZoom());
 setFeatureStyle(soccerFeature, soccerField, map.getView().getZoom());
 setFeatureStyle(tennisFeature, tennis, map.getView().getZoom());
 setFeatureStyle(baseballFeature, baseball, map.getView().getZoom());
+setFeatureStyle(softballFeature, softball, map.getView().getZoom());
 setFeatureStyle(phiDeltaThetaFeature, phidelt, map.getView().getZoom());
+setFeatureStyle(lambdaChiAlphaFeature, lambda, map.getView().getZoom());
+setFeatureStyle(sigmaChiFeature, sigma, map.getView().getZoom());
+setFeatureStyle(fijiFeature, fiji, map.getView().getZoom());
 setFeatureStyle(shoeboxFeature, dining, map.getView().getZoom());
 setFeatureStyle(greenwoodSuitesFeature, residence, map.getView().getZoom());
 setFeatureStyle(dugganLibraryFeature, library, map.getView().getZoom());
@@ -117,6 +136,10 @@ setFeatureStyle(coulterFeature, residence, map.getView().getZoom());
 setFeatureStyle(blytheFeature, residence, map.getView().getZoom());
 setFeatureStyle(donnerFeature, residence, map.getView().getZoom());
 setFeatureStyle(croweFeature, residence, map.getView().getZoom());
+setFeatureStyle(wileyFeature, residence, map.getView().getZoom());
+setFeatureStyle(ideFeature, residence, map.getView().getZoom());
+setFeatureStyle(kpFeature, residence, map.getView().getZoom());
+
 
 
 // Update icon styles on zoom change
@@ -125,7 +148,11 @@ map.on('moveend', () => {
   setIconStyle(stadiumFeature, stadium);
   setIconStyle(tennisFeature, tennis);
   setIconStyle(baseballFeature, baseball);
+  setIconStyle(softballFeature, softball);
   setIconStyle(phiDeltaThetaFeature, phidelt);
+  setIconStyle(lambdaChiAlphaFeature, lambda);  
+  setIconStyle(sigmaChiFeature, sigma);
+  setIconStyle(fijiFeature, fiji);
   setIconStyle(shoeboxFeature, dining);
   setIconStyle(greenwoodSuitesFeature, residence);
   setIconStyle(dugganLibraryFeature, library);
@@ -133,8 +160,11 @@ map.on('moveend', () => {
   setIconStyle(blytheFeature, residence);
   setIconStyle(donnerFeature, residence);
   setIconStyle(croweFeature, residence);
-});
+  setIconStyle(wileyFeature, residence);
+  setIconStyle(ideFeature, residence);
+  setIconStyle(kpFeature, residence);
 
+});
 
 // Popup element
 const popup = document.getElementById('popup');
@@ -220,7 +250,8 @@ const boundingBox = {
 };
 
 const locationsInBBox = [
-  "Culberston Observatory", "Soccer Fields", "Zeddies Tennis Center", "K.T. Young Ballpark", "The Shoebox", "Alumni Stadium", "Phi Delta Theta", "Coulter House", "Lynn Center for Fine Arts", "Greenwood Suites", "File House",
+  "Culberston Observatory", "Soccer Fields", "Zeddies Tennis Center", "K.T. Young Ballpark", "Kops-Bedel Stadium",
+  "The Shoebox", "Alumni Stadium", "Phi Delta Theta", "Coulter House", "Lynn Center for Fine Arts", "Greenwood Suites", "File House",
   "Lambda Chi Fraternity", "Sigma Chi Fraternity", "Charters of Freedom", "Wiley Hall", "Horner Center", "Duggan Library", "Blythe Hall", "Newby Hall",
   "Admission", "IT/Academic Computing", "Crowe Hall", "Lynn Hall", "Faculty Office Building", "Science Center", "Science Hall", "Parker Hall", "Classic Hall",
   "Hendricks Hall", "President's Residence", "Brown Chapel", "Brown Campus Center", "The Quad", "Donner Lawn", "Long Adminstration Building",
@@ -296,7 +327,7 @@ function searchLocation() {
     .then(data => {
       if (data.length > 0) {
         const { lat, lon } = data[0];
-        console.log(lat, lon);
+        console.log(lon, lat);
         // Remove the existing red marker if it exists
         if (currentSearchedFeature) {
           vectorSource.removeFeature(currentSearchedFeature);

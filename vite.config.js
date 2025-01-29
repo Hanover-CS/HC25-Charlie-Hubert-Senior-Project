c715afa0604ee72aeb185328ef9111e52adae779
Charlie Hubert <112531710+CharlieHubert@users.noreply.github.com>
2025-01-29 20:28:43 +0000

    put vite back to the original

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  resolve: {
-    base: "",
-    alias: {
-      'ol': '/node_modules/ol'
-    }
+
+  base: "",
+  build: {
+    sourcemap: true,
   }
 });
